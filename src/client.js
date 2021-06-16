09e121904037b4cc6c5fd06e086697616ae3e532
KituneUdon <kituneudon97@gmail.com>
2021-06-16 14:11:30 +0900

    setup graphql client

---
+++ src/client.js
@@ -0,0 +1,26 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+} from "@apollo/client";
+
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const headersLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+    },
+  });
+  return forward(operation);
+});
+
+const endpoint = "https://api.github.com/graphql";
+const httpLink = new HttpLink({ uri: endpoint });
+const link = ApolloLink.from([headersLink, httpLink]);
+
+export default new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
