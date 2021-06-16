a9549f22c2d10df1ecc1bc250c8683c89713a33f
KituneUdon <kituneudon97@gmail.com>
2021-06-16 17:20:17 +0900

    implement addStar mutation

--- src/StarButton.js
+++ src/StarButton.js
@@ -1,13 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ADD_STAR } from "./graphql";
+import { useMutation } from "@apollo/client";
 
 const StarButton = ({ node }) => {
+  // eslint-disable-next-line
+  const [addStar, { data }] = useMutation(ADD_STAR);
+
   const totalCount = node.stargazers.totalCount;
   const viewerHasStarred = node.viewerHasStarred;
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`;
 
   return (
-    <button type="button">
+    <button
+      type="button"
+      onClick={() =>
+        addStar({ variables: { input: { starrableId: node.id } } })
+      }
+    >
       {starCount} | {viewerHasStarred ? "starred" : "-"}
     </button>
   );
