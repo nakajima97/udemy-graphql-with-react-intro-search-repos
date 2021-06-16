0c128bdd78ed8a42c71bd51343fa94a439f6e526
KituneUdon <kituneudon97@gmail.com>
2021-06-16 14:49:37 +0900

    apply query and variables

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-// import client from "./client";
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
-import { ME } from "./graphql";
+import { SEARCH_REPOSITORIES } from "./graphql";
+
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア",
+};
 
 const App = () => {
-  const { loading, error, data } = useQuery(ME);
+  // eslint-disable-next-line
+  const [variables, setVariables] = useState(VARIABLES);
+
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, { variables });
 
   const showUserName = () => {
     if (loading) return "...Loading";
     if (error) return `Error ${error.message}`;
 
-    return <div>{data.user.name}</div>;
+    return <div>test</div>;
   };
 
   return (
     <>
-      <div>Hello GraphQL</div>
       <div>{showUserName()}</div>
     </>
   );
