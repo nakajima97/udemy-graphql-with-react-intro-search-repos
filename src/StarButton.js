5ec047529d387eb5cc5c51a898480809f3ac8390
KituneUdon <kituneudon97@gmail.com>
2021-06-16 17:31:50 +0900

    implement remove star

--- src/StarButton.js
+++ src/StarButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ADD_STAR } from "./graphql";
+import { ADD_STAR, REMOVE_STAR } from "./graphql";
 import { useMutation } from "@apollo/client";
 
 const StarButton = ({ node }) => {
-  // eslint-disable-next-line
-  const [addStar, { data }] = useMutation(ADD_STAR);
+  const [addStar] = useMutation(ADD_STAR);
+  const [removeStar] = useMutation(REMOVE_STAR);
 
   const totalCount = node.stargazers.totalCount;
   const viewerHasStarred = node.viewerHasStarred;
@@ -15,7 +15,9 @@ const StarButton = ({ node }) => {
     <button
       type="button"
       onClick={() =>
-        addStar({ variables: { input: { starrableId: node.id } } })
+        viewerHasStarred
+          ? removeStar({ variables: { input: { starrableId: node.id } } })
+          : addStar({ variables: { input: { starrableId: node.id } } })
       }
     >
       {starCount} | {viewerHasStarred ? "starred" : "-"}
